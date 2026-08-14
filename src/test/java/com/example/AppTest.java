b24b535fdd9b1351e537e8971d40b806d967cec4
Abhishek Dasgupta <abhishek.dasgupta11@gmail.com>
2026-08-15 00:11:40 +0530

    Create Maven Nexus demo application

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void testMessage() {
+        assertEquals(
+            "Hello from Maven Nexus Demo",
+            App.message()
+        );
+    }
+}
